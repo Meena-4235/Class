0929849cd59f4c9b25b717517e49cf66003a3a84
Meena-4235 <113126864+Meena-4235@users.noreply.github.com>
2022-10-07 21:48:25 +0530

    Update Promote.java

--- Promote.java
+++ Promote.java
@@ -6,7 +6,7 @@ class Promote{
 		int i=5000;
 		float f=5.67f;
 		double d=0.12345;
-		double result=(f*b)+(i/c)-(d*s);
+		double result=(f*b)+(i/c)-(d*s);  //f*b,b is promtoted float and the result of subexpression is converted to float.then,if one operand is long the whole expression is promoted into long.
 		System.out.println((f*b)+"+"+(i/c)+"-"+(d*s));
 		System.out.println("reusult="+result);
 	}
